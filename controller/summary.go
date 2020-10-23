5f303f2002979f1966f628694a46f74d27d18ea9
nano <raynardomongbale@gmail.com>
2020-10-23 13:40:51 -0400

    summary endpoint

--- controller/summary.go
+++ controller/summary.go
@@ -1,18 +1,31 @@
 package controller
 
-import "github.com/labstack/echo"
+import (
+	"Proto/library"
+	"github.com/labstack/echo"
+)
 
 func Summary(c echo.Context) error {
 
-	type test struct {
-		Home string `json:"home"`
-		School string `json:"school"`
-	}
+	workload := library.DataParamsRequest{
 
-
-	testt := &test{
-		Home:   "Montreal",
-		School: "Concordia",
+		BenchmarkType:  "NDBENCH",
+		WorkloadMetric: "CPU",
+		BatchUnit:      2,
+		BatchID:         5,
+		BatchSize:      0,
 	}
-	return c.JSONPretty(201, &testt, "")
+	data, _ := DataSet(c)
+
+	//type test struct {
+	//	Home string `json:"home"`
+	//	School string `json:"school"`
+	//}
+	//
+	//
+	//testt := &test{
+	//	Home:   "Montreal",
+	//	School: "Concordia",
+	//}
+	return c.JSONPretty(201, &data, "")
 }
